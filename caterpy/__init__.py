a2ddb0e7c732392737ad3c9216aaa0da625f1103
kanazuchi <contato@kanazuchi.com>
2018-04-13 20:40:43 +0000

    Some changes

--- caterpy/__init__.py
+++ caterpy/__init__.py
@@ -1,3 +1,4 @@
 from caterpy.get_url_info import * # NOQA
 from caterpy.url_lists import * # NOQA
 from caterpy.learn_category import * # NOQA
+from caterpy.validate_urls import * # NOQA
--- caterpy/validate_urls.py
+++ caterpy/validate_urls.py
@@ -4,42 +4,46 @@
 
 
 import socket
-from glob import glob
 from tldextract import extract
-from collections import UserDict
-
-
-lists = glob('new_lists/*')
-protos = [('HTTP', 80), ('HTTPS', 443)]
+from collections import defaultdict
 
 
 def return_list_dict(list_to_dict):
+    protos = [('HTTP', 80), ('HTTPS', 443)]
+    """Return a dict of urls."""
     urls_list = dict()
     with open(list_to_dict, 'r') as f:
-        urls = [x.strip() for x in f.readlines() if extract(x).registered_domain != '']
+        urls = [x.strip() for x in f.readlines()
+                if extract(x).registered_domain != '']
     for url in urls:
-        e = extrac(url)
+        e = extract(url)
         domain = e.registered_domain
+        print(domain)
         for proto, port in protos:
             try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect(domain, port)
+                s.settimeout(3.0)
+                s.connect((domain, port))
                 if domain not in urls_list.keys():
-                    if urls_list[domain] = defaultdict(lambda: False)
+                    urls_list[domain] = defaultdict(lambda: False)
+                    urls_list[domain]['urls'] = set([])
+                urls_list[domain]['urls'].add(url)
                 if len(url.split(domain)) > 1:
-                    if isinstance(urls_list[domain]['paths'], (list)):
-                        urls_list[domain]['paths'].append(url.split(domain)[-1])
+                    if isinstance(urls_list[domain]['paths'], (set)):
+                        urls_list[domain]['paths'].add(
+                            url.split(domain)[-1])
                     else:
-                        urls_list[domain]['paths'] = [url.split(domain)[-1]]
+                        urls_list[domain]['paths'] = set(
+                            [url.split(domain)[-1]])
                 if e.subdomain != '':
-                    if isinstance(urls_list[domain]['subdomain'], (list)):
-                        urls_list[domain]['subdomain'].append(e.subdomain)
+                    if isinstance(urls_list[domain]['subdomain'], (set)):
+                        urls_list[domain]['subdomain'].add(e.subdomain)
                     else:
-                        urls_list[domain]['subdomain'] = [e.subdomain]
-                if isinstance(urls_list[domain]['protocol'], (list)):
-                    urls_list[domain]['protocol'].append(proto)
+                        urls_list[domain]['subdomain'] = set([e.subdomain])
+                if isinstance(urls_list[domain]['protocol'], (set)):
+                    urls_list[domain]['protocol'].add(proto)
                 else:
-                    urls_list[domain]['protocol'] = [proto]
-            except:
+                    urls_list[domain]['protocol'] = set([proto])
+            except Exception:
                 pass
     return urls_list
