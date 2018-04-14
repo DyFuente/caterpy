9d7b1a4be44f768f94f1b6c69bc973513137ed7b
kanazuchi <contato@kanazuchi.com>
2018-04-14 13:15:32 +0000

    Added tags file

--- caterpy/learn_category.py
+++ caterpy/learn_category.py
@@ -3,14 +3,27 @@
 """Learn category based on a count of words."""
 
 
+from requests_html import HTMLSession
 from caterpy.url_lists import url_lists
 from caterpy.get_url_info import url_info, sum_words
+
+
+def expand_urls(cat_lists):
+    session = HTMLSession()
+    expanded_urls = set([])
+    for url in url_lists[cat_lists]:
+        req = session.get(url)
+        for x in [u.attrs['href'] for u in req.html.find('a')
+                  if 'href' in u.attrs]:
+            if x.startswith("/"):
+                expanded_urls.add(url+x)
+    return expanded_urls
 
 
 def cat_words(cat):
     """Count words of a category based on an url list."""
     words = sum_words()
-    for url in url_lists[cat]:
+    for url in expand_urls(cat):
         try:
             _url_info = url_info(url)
             if _url_info:
