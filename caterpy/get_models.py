b5f37116c041427ef9a287c68d499f9543e39ac8
kanazuchi <contato@kanazuchi.com>
2018-04-22 00:32:13 +0000

    Changed get models

--- caterpy/get_models.py
+++ caterpy/get_models.py
@@ -6,10 +6,9 @@
 from glob import glob
 
 
-def return_models():
+def return_models(limit=-1, diff=False):
     """Return two dicts with model data and model data with unique words."""
     models = glob('classifiers/*')
-    _diff_models = dict()
     _data_models = dict()
 
     for model in models:
@@ -18,12 +17,22 @@ def return_models():
         for w in [word for word, count in list(
                 sorted([(w.split('|')[0], int(w.split('|')[1])) for w in list(
                     filter(None, open(model, 'r').read().split('\n')))],
-                       key=lambda f: f[1], reverse=True))]:
+                       key=lambda f: int(f[1]), reverse=True))]:
             _data_models[name_model].add(w)
 
-    for model in [name for name in _data_models]:
-        union = set.union(*[_data_models[name] for name in _data_models
-                            if name != model])
-        _diff_models[model] = _data_models[model].difference(union)
+    _intersect = set.intersection(
+        *[_data_models[name] for name in _data_models])
 
-    return (_data_models, _diff_models)
+    for model in _data_models:
+        _data_models[model] = set([name for name in _data_models[model]
+                                  if name not in _intersect][0:limit])
+
+    if diff:
+        _diff_models = dict()
+        for model in _data_models:
+            _diff_models[model] = _data_models[model].difference(
+                *[_data_models[name] for name in _data_models
+                  if name != model])
+        return _data_models, _diff_models
+
+    return (_data_models)
