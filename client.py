d5dbaf50712086bb00b5b10b7f9696aa9934bfb1
kanazux <contato@kanazuchi.com>
2018-05-16 20:06:38 +0000

    Added server and client to upload files

---
+++ client.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- codinf: utf-8 -*-
+"""
+Client to send data to socket server.
+
+This client will send urls to be classified.
+"""
+
+
+import sys
+import time
+import socket
+import base64
+
+
+time_file = time.strftime("%Y_%m_%d_%H_%M_%S")
+host = "caterpy.info"  # socket.gethostname()
+port = int(sys.argv[1])
+get_file = sys.argv[2]
+file_name = socket.gethostname()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+key = base64.b64encode('caterpy_classify_url'.encode())
+
+with client:
+    client.send(key)
+    get_data = client.recv(1024)
+    if get_data == "next".encode():
+        client.send("{}-{}".format(file_name, time_file).encode())
+    get_data = client.recv(1024)
+    if get_data == "send_file".encode():
+        read_file = open(get_file, 'rb')
+        get_data = read_file.read()
+        client.send(get_data)
---
+++ server.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Collect data from clients with urls to classify.
+
+This server daemon will use port 60001 to listen connections.
+Will save data with date and name of client.
+"""
+
+
+import sys
+import socket
+import base64
+
+
+host = "caterpy.info" # socket.gethostname()
+port = int(sys.argv[1])
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host, port))
+sock.listen(5)
+
+key = base64.b64encode('caterpy_classify_url'.encode())
+
+
+def save_file(file_name, data_file, source_ip, source_port):
+    _name = "{}-{}".format(source_ip, file_name)
+    print("Connection with IP: {} in the port {}!".format(
+        source_ip, source_port))
+    print("Saving file on files/files_to_classify/{}".format(_name))
+    with open("files/files_to_classify/{}".format(_name), "w") as s_file:
+        s_file.write("".join(data_file))
+
+
+while True:
+    try:
+        conn, addr = sock.accept()
+        with conn:
+            get_data = conn.recv(1024)
+            if get_data == key:
+                conn.send("next".encode())
+                get_file_name = conn.recv(1024)
+                if get_file_name != b"":
+                    conn.send("send_file".encode())
+                    data = []
+                    get_data = conn.recv(1024)
+                    while get_data != b"":
+                        data.append(get_data.decode())
+                        get_data = conn.recv(1026)
+                save_file(get_file_name.decode(), data, addr[0], addr[1])
+    except Exception as error:
+        print(error)
