4dda5187d862d70836879e9345378ae8586fa844
root <root@localhost.localdomain>
2018-09-20 19:56:39 -0300

    change emperor

--- web_site/scripts/emperor.sh
+++ web_site/scripts/emperor.sh
@@ -8,6 +8,7 @@ UWSGI="/usr/local/www/caterpy/bin/uwsgi"
 VASSALS="/usr/local/etc/"
 LOG="/var/log/uwsgi/emperor_caterpy.log"
 
+source /usr/local/www/caterpy/bin/activate
 $UWSGI --emperor $VASSALS --uid www-data --gid www-data --daemonize $LOG
 
 service nginx restart
