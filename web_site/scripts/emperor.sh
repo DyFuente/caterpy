1ddeda0097dd6f7bb5a81306a700b45feb8b91bf
kanazux <contato@kanazuchi.com>
2018-09-20 15:21:39 -0300

    Update some files for web pages

--- web_site/scripts/emperor.sh
+++ web_site/scripts/emperor.sh
@@ -2,12 +2,12 @@
 # Start new process of uwsgi and restart nginx.
 
 PROC=`pgrep -fl uwsgi`
-[ -n "${PROC}" ] && pkill -9 -af uwsgi; sleep 1
+[ -n "${PROC}" ] && pkill -9 -f uwsgi; sleep 1
 
-UWSGI="/usr/local/www/bin/uwsgi"
-VASSALS="/usr/local/etc/nginx/uwsgi/vassals"
+UWSGI="/usr/local/www/caterpy/bin/uwsgi"
+VASSALS="/usr/local/etc/"
 LOG="/var/log/uwsgi/emperor_caterpy.log"
 
-$UWSGI --emperor $VASSALS --uid www --gid www --daemonize $LOG
+$UWSGI --emperor $VASSALS --uid www-data --gid www-data --daemonize $LOG
 
 service nginx restart
